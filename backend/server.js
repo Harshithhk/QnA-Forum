5d85a340ac9be20340c0b3e709ffa8735cf96dc1
Harshithhk <60171634+Harshithhk@users.noreply.github.com>
2020-10-11 21:54:59 +0530

    initialized express server

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require ('express')
+const dotenv = require('dotenv')
+const app = express()
+
+dotenv.config()
+
+app.get("/", (req,res)=>{
+    res.send('HELLO FROM API')
+})
+
+const PORT = process.env.PORT || 5000
+
+
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
