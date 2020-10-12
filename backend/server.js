8e9e4b5eb9167fe9bfe4bd1bc54e73d6997dbcb6
Harshithhk <60171634+Harshithhk@users.noreply.github.com>
2020-10-12 12:21:25 +0530

    Revert "added questions routes"
    This reverts commit 36e5b4f61d45a899f284df3ed7325e57d9a58210.

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,15 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
-import colors from 'colors'
-
-import questionRoutes from './routes/questionRoutes.js'
-
+const express = require ('express')
+const dotenv = require('dotenv')
 const app = express()
 
 dotenv.config()
-
-connectDB()
 
 app.get("/", (req,res)=>{
     res.send('HELLO FROM API')
 })
 
-app.use('/api/questions',questionRoutes)
-
 const PORT = process.env.PORT || 5000
 
 
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.cyan.underline.bold))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
